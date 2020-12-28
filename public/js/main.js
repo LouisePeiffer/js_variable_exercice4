e9ce392bb3282c7dc9a9bb6f215a3e4fc0bbf7b6
Louise <louisepeiffer@hotmail.com>
2020-12-28 16:11:30 +0100

    2 exos faits

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+// Exo 1
+//let nom = "Peiffer"
+//let prenom = "Louise"
+//let ville = prompt('Ville')
+//alert (`${ville}`)
+
+// Exo 2
+// a)
+//let nom = "Peiffer"
+//let prenom = "Louise"
+//let ville = "Bxl"
+//let desti = "Montréal"
+
+//console.log(`Le voayage de ${ville} a ${desti} a été enregistré au nom de ${nom} ${prenom}`);
+
+// b)
+let nom = prompt('Nom')
+let prenom = prompt('Prénom')
+let ville1 = prompt('Ville')
+let ville2 = prompt('Ville 2')
+
+console.log(`Le voayage de ${ville1} a ${ville2} a été enregistré au nom de ${nom} ${prenom}`);
